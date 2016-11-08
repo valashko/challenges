49f909d068482ed9012cc0823e250e0f5508b487
Volodymyr Lashko <volodymyr.lashko@gmail.com>
2016-11-08 21:08:35 +0200

    Add example for set() in main

--- prism/image-library/main.cpp
+++ prism/image-library/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <stdexcept>
 #include <string>
 
@@ -9,9 +10,11 @@ using namespace Prism;
 int main()
 {
   std::vector< int > greyPixels = { 0, 1, 2, 3, 4 };
-  const Image & greyImage = GreyImageInt(5, 1, greyPixels);
-  std::cout << "w: " << greyImage.width() << ", h: " << greyImage.height() << std::endl;
-  std::cout << "get(4,0) = " << greyImage.get(4, 0).cast< int >() << std::endl;
+  std::unique_ptr< Image > greyImage(new GreyImageInt(5, 1, greyPixels));
+  std::cout << "w: " << greyImage->width() << ", h: " << greyImage->height() << std::endl;
+  std::cout << "get(4,0) = " << greyImage->get(4, 0).cast< int >() << std::endl;
+  greyImage->set(4, 0, Pixel(42));
+  std::cout << "after modification get(4,0) = " << greyImage->get(4, 0).cast< int >() << std::endl;
 
   return 0;
 }
